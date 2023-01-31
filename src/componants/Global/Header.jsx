a2e73008ef6b23e4f8d50f48fabcbca5d4523ea9
Florian <floriangreco@hotmail.com>
2023-01-31 15:08:01 +0100

    fix head

--- src/componants/Global/Header.jsx
+++ src/componants/Global/Header.jsx
@@ -6,10 +6,10 @@ export default function header () {
     <h1 className='h1'>La table de Chantal</h1>
 
     <ul className="navbar">
-      <a className="lien" href="#"><li>Acceuil</li></a>
-      <a className="lien" href="#"><li>Contact</li></a>
-      <a className="lien" href="#"><li>Boissons</li></a>
-      <a className="lien" href="#"><li>Plats</li></a>
+      <a className="lien" href="#"><li className='liheader'>Acceuil</li></a>
+      <a className="lien" href="#"><li className='liheader'>Contact</li></a>
+      <a className="lien" href="#"><li className='liheader'>Boissons</li></a>
+      <a className="lien" href="#"><li className='liheader'>Plats</li></a>
     </ul>
   </header>
     )
